09f0c33ccc0accc318194c27f52d39da5717e295
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-10-06 17:42:20 +0000

    cleared master branch

--- errors.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERRORS_H
-#define ERRORS_H
-
-
-void _perror_exit(char *arg);
-void command_not_found(char *arg);
-void _perror_file(char *arg, char *str);
-#endif
--- prints.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PRINTS_H
-#define _PRINTS_H
-
-#include <stddef.h>
-
-int _putchar(char c);
-size_t _strlen(const char *str);
-#endif
